86a17a711cdad01ed1c7ac94af6c4dfbec355cfe
inficoder <inficoder@gmail.com>
2020-09-30 16:22:27 +0800

    fist

---
+++ eureka-client/src/main/java/com/cloud/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,22 @@
+package com.cloud.eurekaclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 通过注解@EnableEurekaClient 表明自己是一个eurekaclient.
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
